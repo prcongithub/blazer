3e4b1f13bd863d7b96e9a3dccbcdcd7cb07982a1
Prashant <prashant.chaudhari89@gmail.com>
2022-02-11 13:23:09 +0530

    make uploads connection a regular active record class
    This is done to fix the connection switch issues for multi db apps

--- app/models/blazer/uploads_connection.rb
+++ app/models/blazer/uploads_connection.rb
@@ -1,7 +1,7 @@
 module Blazer
   class UploadsConnection < ActiveRecord::Base
-    self.abstract_class = true
+    #self.abstract_class = true
 
-    establish_connection Blazer.settings["uploads"]["url"] if Blazer.uploads?
+    #establish_connection Blazer.settings["uploads"]["url"] if Blazer.uploads?
   end
 end
